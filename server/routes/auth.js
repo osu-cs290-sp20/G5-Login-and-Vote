66e53f5d59531f695ab9c028e865d3597fffa365
EndePointe <endepointe@gmail.com>
2020-05-13 22:02:26 +0100

    added mongodb user with password hashing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../model/User');
 let crypto;
 let hasC = false;
+
 try {
   crypto = require('crypto');
   hasC = true;
@@ -14,8 +15,14 @@ router.post('/register', async (req, res) => {
 
   if (hasC) {
     const user = new User({
-      name: req.body.name,
-      email: req.body.email,
+      name: crypto
+        .createHash('sha256')
+        .update(req.body.name)
+        .digest('hex'),
+      email: crypto
+        .createHash('sha256')
+        .update(req.body.email)
+        .digest('hex'),
       password: crypto
         .createHash('sha256')
         .update(req.body.password)
