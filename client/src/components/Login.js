d518953573b566212527c70c437dc916c9fd05a2
EndePointe <endepointe@gmail.com>
2020-05-14 16:31:37 +0100

    indent

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,28 +5,28 @@ import './Login.css';
 
 const Login = () => {
   return (
-<Form className="loginForm">
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+    <Form className="loginForm">
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone.
+        </Form.Text>
+      </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <div className="loginBtns">
-  <Button className="loginBtn" variant="primary" type="submit">
-    Login 
-  </Button>
-  <Button className="loginBtn" variant="primary" type="submit">
-    Register 
-  </Button>
-  </div>
-</Form>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <div className="loginBtns">
+        <Button className="loginBtn" variant="primary" type="submit">
+          Login
+      </Button>
+        <Button className="loginBtn" variant="primary" type="submit">
+          Register
+      </Button>
+      </div>
+    </Form>
   );
 };
 
