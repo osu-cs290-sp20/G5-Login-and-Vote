153ecacb941c9ddd402d748e3e90e91bf3ee1456
EndePointe <alvin.johnsii@gmail.com>
2020-05-14 01:56:55 -0700

    added login/register form

---
+++ client/src/components/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './Login.css';
+
+const Login = () => {
+  return (
+<Form className="loginForm">
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <div className="loginBtns">
+  <Button className="loginBtn" variant="primary" type="submit">
+    Login 
+  </Button>
+  <Button className="loginBtn" variant="primary" type="submit">
+    Register 
+  </Button>
+  </div>
+</Form>
+  );
+};
+
+export default Login;
