da72cde7ecc00c5af0b34bf0ff906b7b8a1df4f0
EndePointe <endepointe@gmail.com>
2020-05-17 02:39:30 +0100

    testing merge

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,11 +4,18 @@ import Button from 'react-bootstrap/Button';
 import './Login.css';
 
 const Login = () => {
+
+
+  const handleSubmit = (e) => {
+    // send value to /api/user/register
+    // api/user/login
+  }
+
   return (
-    <Form className="loginForm">
+    <Form onSubmit={handleSubmit} className="loginForm">
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control id="input" type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone.
         </Form.Text>
@@ -19,7 +26,8 @@ const Login = () => {
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
       <div className="loginBtns">
-        <Button className="loginBtn" variant="primary" type="submit">
+        <Button
+          className="loginBtn" variant="primary" type="submit">
           Login
       </Button>
         <Button className="loginBtn" variant="primary" type="submit">
