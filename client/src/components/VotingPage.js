d0633d17148282b50b39c0940f0d5acc405e3f38
EndePointe <endepointe@gmail.com>
2020-05-26 17:39:57 +0100

    login/reg flows to voting page

--- client/src/components/VotingPage.js
+++ client/src/components/VotingPage.js
@@ -10,6 +10,12 @@ const VotingPage = (props) => {
         <h1>Vote page</h1>
         <p>Status: {props.auth}</p>
         <p>User: {props.user}</p>
+        <h5>to do items</h5>
+        <ul>
+          <li>display user names - found in the props.user in this component</li>
+          <li>add the list of measures as a sisde item</li>
+          <li>consider continuing the use of react router</li>
+        </ul>
       </div>
     </div>
   );
